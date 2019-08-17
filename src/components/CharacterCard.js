54fc50e3b356ba1badfb83d9fe224e2008d6e6a2
Asha <ashalbenjamin@gmail.com>
2019-08-16 21:23:38 -0500

    added-episode-list

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard ({ name, image, location, origin, species, status, id }) {
+
+  return (
+    <Card
+      
+    />
+  )
 }
